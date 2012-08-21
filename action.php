809fcb72c6b6ef97a13a22d40124b98330728d09
Andreas Gohr <gohr@cosmocode.de>
2012-08-21 13:53:07 +0200

    initial wizard created checkin

---
+++ action.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * DokuWiki Plugin pagehere (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Andreas Gohr <gohr@cosmocode.de>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die();
+
+if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
+if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+
+require_once DOKU_PLUGIN.'action.php';
+
+class action_plugin_pagehere extends DokuWiki_Action_Plugin {
+
+    public function register(Doku_Event_Handler &$controller) {
+
+       $controller->register_hook('DOKUWIKI_STARTED', 'FIXME', $this, 'handle_dokuwiki_started');
+   
+    }
+
+    public function handle_dokuwiki_started(Doku_Event &$event, $param) {
+    }
+
+}
+
+// vim:ts=4:sw=4:et:
