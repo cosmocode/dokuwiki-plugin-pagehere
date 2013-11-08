58d7efd565a9cd4584d1ca40c2602b9a28443560
Mark Prins <mprins@users.sf.net>
2013-11-08 18:55:20 +0100

    fix @author tag..
    and use commanding/active sense

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -1,13 +1,9 @@
 <?php
 /**
- * Dutch language file for pagehere plugin
+ * Dutch language file for pagehere plugin.
  *
  * @author Andreas Gohr <gohr@cosmocode.de>
- * Mark C. Prins <mprins@users.sf.net>
+ * @author Mark C. Prins <mprins@users.sf.net>
  */
 
-$lang['submit'] = 'Maak een nieuwe pagina aan';
-
-
-
-//Setup VIM: ex: et ts=4 :
+$lang['submit'] = 'Nieuwe pagina aanmaken';
