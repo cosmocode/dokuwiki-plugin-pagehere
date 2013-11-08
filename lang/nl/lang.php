78da8875375111fb235ff629530769d0b584e056
Mark Prins <mprins@users.sf.net>
2013-11-08 18:47:57 +0100

    create dutch translation of lang.php

---
+++ lang/nl/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Dutch language file for pagehere plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ * Mark C. Prins <mprins@users.sf.net>
+ */
+
+$lang['submit'] = 'Maak een nieuwe pagina aan';
+
+
+
+//Setup VIM: ex: et ts=4 :
