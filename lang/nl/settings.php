c14d9da33f9914f179907c9aedee8e9077d2b1bc
Mark Prins <mprins@users.sf.net>
2013-11-08 18:52:19 +0100

    create dutch translation of settings.php

---
+++ lang/nl/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Dutch language file for pagehere plugin.
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ * @author Mark C. Prins <mprins@users.sf.net>
+ */
+
+
+$lang['subns'] = 'Sta het gebruik van het naamruimte scheidingsteken (:) om nieuwe naamruimtes aan te maken';
